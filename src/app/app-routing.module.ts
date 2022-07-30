c4fe84f182b0094dadaecec5d10b9ce3ca11482a
Ronal Mendoza <ronalmendozat@outlook.com>
2022-07-30 16:16:40 -0500

    menu con angular terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrarComponent } from './registrar/registrar.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:"registrar-link", component:RegistrarComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
